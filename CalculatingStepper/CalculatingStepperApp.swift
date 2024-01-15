b1b0dcc285623b7a1664ddd0a4ff9cec53a34fe3
Sophie Haber <SHaber@lcs.on.ca>
2024-01-15 11:26:49 -0500

    Initial Commit

---
+++ CalculatingStepper/CalculatingStepperApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatingStepperApp.swift
+//  CalculatingStepper
+//
+//  Created by Sophie Haber on 15.01.24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatingStepperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
