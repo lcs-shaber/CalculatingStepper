f58eb6bdf153fdb6a42899ab214446a93633105e
Sophie Haber <SHaber@lcs.on.ca>
2024-01-15 11:35:11 -0500

    I created the first two numbers.

--- CalculatingStepper/ContentView.swift
+++ CalculatingStepper/ContentView.swift
@@ -8,12 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Stored properties
+    @State var firstNumber: Int = 1
+    @State var secondNumber: Int = 1
+    
+    // MARK: Computed properties
+    var sum: Int {
+        return firstNumber + secondNumber
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            HStack{
+                Spacer()
+                
+                Text("\(firstNumber)")
+                    .padding()
+            }
+            
+            Stepper(value: $firstNumber, label: {
+                Text("select first number")
+            })
+            
+            HStack{
+                Text("+")
+                    .padding()
+                
+                Spacer()
+                
+                Text("\(secondNumber)")
+                    .padding()
+            }
+            
+            Stepper(value: $secondNumber, label: {
+                Text("select second number")
+            })
+           
+            
         }
         .padding()
     }
