1f5ba5344f5610bbf006e3fe88a0f25ffc8d0d94
Sophie Haber <SHaber@lcs.on.ca>
2024-01-15 12:00:02 -0500

    I finished the multiplying and added a view for dividing

---
+++ CalculatingStepper/Divide.swift
@@ -0,0 +1,18 @@
+//
+//  Divide.swift
+//  CalculatingStepper
+//
+//  Created by Sophie Haber on 15.01.24.
+//
+
+import SwiftUI
+
+struct Divide: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Divide()
+}
--- CalculatingStepper/Multiply.swift
+++ CalculatingStepper/Multiply.swift
@@ -8,10 +8,61 @@
 import SwiftUI
 
 struct Multiply: View {
+    
+    // MARK: Stored properties
+    @State var firstNumber: Int = 1
+    @State var secondNumber: Int = 1
+    
+    // MARK: Computed properties
+    var sum: Int {
+        return firstNumber * secondNumber
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            
+            HStack{
+                Spacer()
+                
+                Text("\(firstNumber)")
+                    .padding()
+                    .font(.system(size: 60))
+            }
+            
+            Stepper(value: $firstNumber, label: {
+                Text("select first number")
+            })
+            
+            HStack{
+                Text("x")
+                    .font(.system(size: 60))
+                    .padding()
+                
+                Spacer()
+                
+                Text("\(secondNumber)")
+                    .font(.system(size: 60))
+                    .padding()
+            }
+            
+            Stepper(value: $secondNumber, label: {
+                Text("select second number")
+            })
+            
+            HStack{
+                Spacer()
+                
+                Text("\(sum)")
+                    .font(.system(size: 60))
+                    .padding()
+            }
+            
+        }
+        .padding()
     }
 }
+
 
 #Preview {
     Multiply()
